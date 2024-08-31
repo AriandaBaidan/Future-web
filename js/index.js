275c463b3dd43c017970311c786b9a5366511d71
Arianda Baidan <arianda.baidan@gmail.com>
2024-08-31 21:33:37 +0300

    Pilna musu nauja svetaine

---
+++ js/index.js
@@ -0,0 +1,21 @@
+console.log("Arianda");
+
+var vardas = "Ariandos vardas";
+
+// String tipas
+let pavarde = "Baidan";
+
+// Number tipas
+const gimimoMetai = 1996;
+
+//Bollem tipas-loginis tipas
+let arJusSuauges = true;
+let arJusVaikas = false;
+
+// Undefined tipas
+let nepriskirtaReiksme;
+
+console.log(nepriskirtaReiksme);
+nepriskirtaReiksme = "Tekstas 1";
+
+let tusciasKintamas = null;
